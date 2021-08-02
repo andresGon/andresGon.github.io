1f924c5e59aef0c66f65475045e768e3e8f50594
Andres Gonzalez <andres.gonzalez@dasigno.com>
2021-08-01 21:05:03 -0500

    implementacion de dos idiomas

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 
+// ================= funcion dia noche ================
 
+var body = document.getElementsByTagName('body')[0];
 var btnDay = document.getElementById('btnDay');
 var btnNight = document.getElementById('btnNight');
 var mainDiv = document.getElementById('mainDiv');
@@ -11,3 +13,39 @@ btnDay.addEventListener('click', function(){
 btnNight.addEventListener('click', function(){
     mainDiv.classList.add('night');
 })
+
+
+// =======  local storage ==========
+
+var btnEsp = document.getElementById('esp');
+var btnEng = document.getElementById('eng');
+
+localStorage.setItem('idioma', 'eng')
+
+btnEsp.addEventListener('click', function(){
+    idiomaCambio('esp');
+});
+
+
+btnEng.addEventListener('click', function(){
+    idiomaCambio('eng');
+});
+
+var idiomaSet = localStorage.getItem('idioma');
+
+function idiomaCambio(valIdioma){
+    if(valIdioma === 'esp'){
+        localStorage.setItem('idioma', 'esp');
+        console.log('deneria estar en español')
+        body.classList.add('esp');
+    }
+    if(valIdioma === 'eng'){
+        localStorage.setItem('idioma', 'eng');
+        console.log('deberia estar en ingles')
+        body.classList.remove('esp');
+    }
+}
+
+idiomaCambio();
+
+
