6b385dcc10a07e4752a82235c9ca707ca50c66ec
Andres Gonzalez <andres.gonzalez@dasigno.com>
2021-08-04 22:21:21 -0500

    ajustes en menu y boton night

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@ var btnDay = document.getElementById('btnDay');
 var btnNight = document.getElementById('btnNight');
 var mainDiv = document.getElementById('mainDiv');
 
-btnDay.addEventListener('click', function(){
-    mainDiv.classList.remove('night');
-});
+// btnDay.addEventListener('click', function(){
+//     mainDiv.classList.remove('night');
+// });
 
 btnNight.addEventListener('click', function(){
-    mainDiv.classList.add('night');
+    mainDiv.classList.toggle('night');
 })
 
 
